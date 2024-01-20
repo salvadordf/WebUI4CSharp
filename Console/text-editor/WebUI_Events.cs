b82ef0b46332801885e3c3c8157525a6de0f62ea
Salvador Díaz Fau <briskbard@briskbard.com>
2024-01-20 12:32:04 +0100

    Added text-editor demo

---
+++ Console/text-editor/WebUI_Events.cs
@@ -0,0 +1,12 @@
+﻿using WebUI4CSharp;
+
+namespace text_editor
+{
+    public static class WebUI_Events
+    {
+        public static void Close(ref webui_event_t e)
+        {
+            WebUI.Exit();
+        }
+    }
+}
